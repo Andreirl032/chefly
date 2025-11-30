d4484d3712af7278fe4dd3b62d4a63c114f1cc0b
Andreirl032 <andrei.ramos.lopes@gmail.com>
2025-11-30 05:00:26 -0300

    added drawer to switch pages

--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import FeedScreen from '../screens/App/FeedScreen.tsx';
-import RecipeDetailsScreen from '../screens/App/RecipeDetailsScreen.tsx';
-import FavoritesScreen from '../screens/App/FavoritesScreen.tsx';
-import OfflineSavedScreen from '../screens/App/OfflineSavedScreen.tsx';
-import ProfileScreen from '../screens/App/ProfileScreen.tsx';
+// import FeedScreen from '../screens/App/FeedScreen.tsx';
+// import RecipeDetailsScreen from '../screens/App/RecipeDetailsScreen.tsx';
+// import FavoritesScreen from '../screens/App/FavoritesScreen.tsx';
+// import OfflineSavedScreen from '../screens/App/OfflineSavedScreen.tsx';
+// import ProfileScreen from '../screens/App/ProfileScreen.tsx';
+import DrawerNavigator from './DrawerNavigator.tsx';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,16 +13,18 @@ export default function AppStack() {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerStyle: { backgroundColor: '#FF7A00' },
-        headerTintColor: '#fff',
-        headerTitleStyle: { fontWeight: 'bold' },
+        // headerStyle: { backgroundColor: '#FF7A00' },
+        // headerTintColor: '#fff',
+        // headerTitleStyle: { fontWeight: 'bold' },
+        headerShown: false,
       }}
     >
-      <Stack.Screen name="Feed" component={FeedScreen} />
+      {/* <Stack.Screen name="Feed" component={FeedScreen} />
       <Stack.Screen name="Recipe" component={RecipeDetailsScreen} />
       <Stack.Screen name="Favorites" component={FavoritesScreen} />
       <Stack.Screen name="Offline Saved" component={OfflineSavedScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+      <Stack.Screen name="AppDrawer" component={DrawerNavigator} />
     </Stack.Navigator>
   );
 }
---
+++ src/navigation/DrawerNavigator.tsx
@@ -0,0 +1,76 @@
+// src/navigation/DrawerNavigator.tsx
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FeedScreen from '../screens/App/FeedScreen';
+import FavoritesScreen from '../screens/App/FavoritesScreen';
+import OfflineScreen from '../screens/App/OfflineSavedScreen';
+import ProfileScreen from '../screens/App/ProfileScreen';
+import SettingsScreen from '../screens/App/SettingsScreen';
+// import MaterialIcons from '@react-native-vector-icons/material-icons';
+
+const Drawer = createDrawerNavigator();
+
+// const createIcon =
+//   (name: any) =>
+//   ({ color, size }: { color: string; size: number }) =>
+//     <MaterialIcons name={name} size={size} color={color} />;
+
+export default function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: true,
+        drawerActiveTintColor: '#FF7F50',
+        drawerLabelStyle: { fontSize: 16 },
+        headerStyle: { backgroundColor: '#FF7A00' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: 'bold' },
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={FeedScreen}
+        options={{
+          title: 'Feed',
+          // drawerIcon: createIcon("restaurant-menu"),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          title: 'Favoritos',
+          // drawerIcon: createIcon("favorite"),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Offline"
+        component={OfflineScreen}
+        options={{
+          title: 'Offline',
+          // drawerIcon: createIcon("vloud-off"),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: 'Perfil',
+          // drawerIcon: createIcon("person"),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          title: 'Configurações',
+          // drawerIcon: createIcon("settings"),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
